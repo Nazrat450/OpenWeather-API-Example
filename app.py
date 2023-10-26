c07ef17dd1f544897d6aca2056a4919cd67fdf16
Nazrat450 <145197951+Nazrat450@users.noreply.github.com>
2023-10-27 00:13:56 +1100

    Added stackable table results

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
-
 
 ######MatthewHill#####################
 ##                                  ##   
@@ -8,10 +7,11 @@ import requests
 ##                                  ##
 ######################################
 app = Flask(__name__)
-
+weather_data = []  # Global variable to store weather data
+error_message = None  # Variable to store the error message
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    weather_data = []
+    global weather_data  # Use the global variable
     # Locations defined by assignment
     locations = [
         {"name": "Lake District National Park", "lat": 54.4609, "lon": -3.0886},
@@ -26,36 +26,43 @@ def index():
         {"name": "Birmingham", "lat": 52.4862, "lon": -1.8904}
     ]
     #Search Bar
+    existing_locations = {entry['location'] for entry in weather_data} 
     if request.method == 'POST':
         user_location = request.form.get('location')
-        if user_location:
-            # API from Open Weather
+        if user_location and user_location not in existing_locations:
             api_url = f"https://api.openweathermap.org/data/2.5/weather?q={user_location}&appid=42c10c6eeff25fdb9a2e1ae45c758f7f&units=metric"
             response = requests.get(api_url)
             if response.status_code == 200:
                 current_weather = response.json()
-                # Clear
-                weather_data.clear()
                 weather_data.append({
                     'location': user_location,
                     'temperature': current_weather['main']['temp'],
                     'condition': current_weather['weather'][0]['description']
-                }) 
+                })
+        return render_template('index.html', weather_data=weather_data)
 
-    for location in locations:
-        lat, lon = location['lat'], location['lon']
-        # API from Open Weather
-        api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=42c10c6eeff25fdb9a2e1ae45c758f7f&units=metric"
-        response = requests.get(api_url)
-        if response.status_code == 200:
-            current_weather = response.json()
-            weather_data.append({
-                'location': location['name'],
-                'temperature': current_weather['main']['temp'],
-                'condition': current_weather['weather'][0]['description']
-            })
+    if request.method == 'GET':
+        for location in locations:
+            if location['name'] not in existing_locations:
+                lat, lon = location['lat'], location['lon']
+                api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=42c10c6eeff25fdb9a2e1ae45c758f7f&units=metric"
+                response = requests.get(api_url)
+                if response.status_code == 200:
+                    current_weather = response.json()
+                    weather_data.append({
+                        'location': location['name'],
+                        'temperature': current_weather['main']['temp'],
+                        'condition': current_weather['weather'][0]['description']
+                    })
+                    
 
     return render_template('index.html', weather_data=weather_data)
+
+@app.route('/clear', methods=['GET'])
+def clear_data():
+    global weather_data
+    weather_data.clear()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
